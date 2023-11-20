bae34bfdd5ed513248771ebb6cfcace47f2bc4fc
JKirkis <jordkerkvliet@gmail.com>
2023-11-20 15:34:13 -0600

    Connection string

--- MVCFinalProject/Program.cs
+++ MVCFinalProject/Program.cs
@@ -1,7 +1,12 @@
+using MVCFinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<GameContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GameContext")));
 
 var app = builder.Build();
 
