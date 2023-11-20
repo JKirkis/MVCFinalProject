14ce02a2155d5e7af25f0b134a6eaa9489eb7982
JKirkis <jordkerkvliet@gmail.com>
2023-11-20 15:11:47 -0600

    Game model

--- MVCFinalProject/Models/Game.cs
+++ MVCFinalProject/Models/Game.cs
@@ -1,6 +1,26 @@
-﻿namespace MVCFinalProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCFinalProject.Models
 {
     public class Game
     {
+        public int GameId { get; set; }
+
+        [Required(ErrorMessage = "Please eneter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1950, 2999, ErrorMessage = "Year must be after 1950.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a developer.")]
+        public int? Developer { get; set; }
+
+        [Required(ErrorMessage = "Please enter a genre.")]
+        public string Genre { get; set; }
+
+        public string Slug =>
+          Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
     }
+}
 }
