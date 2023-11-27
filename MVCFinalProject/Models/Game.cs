c17959220098e92c9f2e9ddd8b82787f6efd78e2
JKirkis <jordkerkvliet@gmail.com>
2023-11-27 13:26:09 -0600

    WHY ISN'T DELETE WORKING

--- MVCFinalProject/Models/Game.cs
+++ MVCFinalProject/Models/Game.cs
@@ -5,20 +5,20 @@ namespace MVCFinalProject.Models
     public class Game
     {
         [Key]
-        public int GameId { get; set; }
+        public int? GameId { get; set; }
 
         [Required(ErrorMessage = "Please eneter a name.")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required(ErrorMessage = "Please enter a year.")]
         [Range(1950, 2999, ErrorMessage = "Year must be after 1950.")]
         public int? Year { get; set; }
 
         [Required(ErrorMessage = "Please enter a developer.")]
-        public string Developer { get; set; }
+        public string? Developer { get; set; }
 
         [Required(ErrorMessage = "Please enter a genre.")]
-        public string Genre { get; set; }
+        public string? Genre { get; set; }
 
         public string Slug =>
           Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
